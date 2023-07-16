cca9fdb2950ab22d11cfa151d1456b670c6f0093
H43apo8 <118731185+H43apo8@users.noreply.github.com>
2023-07-16 12:14:39 +0300

    Update homework_006_SQL.sql

--- homework_006_SQL.sql
+++ homework_006_SQL.sql
@@ -4,46 +4,45 @@ use homework_database_006;
 -- 1.	Создайте функцию, которая принимает кол-во сек и форматирует их в кол-во дней, часов, минут и секунд.
 -- Пример: 123456 ->'1 days 10 hours 17 minutes 36 seconds '
 
-DELIMITER //
-CREATE PROCEDURE second_counter(num INT)
+CREATE FUNCTION formatSeconds(seconds INT)
+RETURNS VARCHAR(100)
 BEGIN
-	CASE
-		WHEN num < 60 THEN
-			SELECT CONCAT(num, ' ', 'seconds') AS Result;
-        WHEN num >= 60 AND num < 3600 THEN
-			SELECT CONCAT_WS(' ', num DIV 60, 'minutes', MOD(num, 60), 'seconds') AS Result;
-        WHEN num >= 3600 AND num < 86400 THEN
-			SELECT CONCAT_WS(' ', num DIV 3600, 'hours', MOD(num, 3600) DIV 60, 'minutes', MOD(MOD(num, 3600),60), 'seconds') AS Result;
-        ELSE
-			SELECT CONCAT_WS(' ', num DIV 86400, 'days', MOD(num, 86400) DIV 3600, 'hours', MOD(MOD(num, 86400),3600) DIV 60, 'minutes',
-                             MOD(MOD(MOD(num, 86400),3600),60), 'seconds') AS Result;
-    END CASE;
-END//
+    DECLARE days INT;
+    DECLARE hours INT;
+    DECLARE minutes INT;
+    DECLARE formattedTime VARCHAR(100);
+    
+    SET days = seconds / (24 * 60 * 60);
+    SET seconds = seconds % (24 * 60 * 60);
+    
+    SET hours = seconds / (60 * 60);
+    SET seconds = seconds % (60 * 60);
+    
+    SET minutes = seconds / 60;
+    SET seconds = seconds % 60;
+    
+    SET formattedTime = CONCAT(days, ' days ', hours, ' hours ', minutes, ' minutes ', seconds, ' seconds');
+    
+    RETURN formattedTime;
+END;
 
-DELIMITER ;
-CALL second_counter(123456);  
+SELECT formatSeconds(123456);
 
--- 2.	Выведите только четные числа от 1 до 10 включительно.
--- Пример: 2,4,6,8,10 (можно сделать через шаг +  2: х = 2, х+=2)
+-- 2. Создайте процедуру, которая выводит только четные числа от 1 до 10. Пример: 2,4,6,8,10
 
-DELIMITER //
-CREATE PROCEDURE get_even(`start` INT, `end` INT)
+CREATE PROCEDURE getEvenNumbers()
 BEGIN
-	DECLARE i INT DEFAULT `start`;
-    DECLARE res_str TEXT DEFAULT NULL;
-    WHILE  i<=`end` DO
-        IF i%2 = 0 THEN
-			IF res_str IS NULL THEN
-				SET res_str = concat(i);
-			ELSE
-				SET res_str = concat(res_str, ', ', i);
-			END IF;
-		END IF;
+    DECLARE i INT;
+    
+    SET i = 2;
+    
+    WHILE i <= 10 DO
+        IF i % 2 = 0 THEN
+            SELECT i;
+        END IF;
+        
         SET i = i + 1;
     END WHILE;
-	SELECT res_str;
-END //
-DELIMITER ;
+END;
 
-CALL get_even(1, 10);
-CALL second_counter(123456);  
+CALL getEvenNumbers();
